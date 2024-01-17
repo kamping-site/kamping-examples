466bb30f138221da30f6e86be76908f81a2c8382
Tim Niklas Uhl <uhl@kit.edu>
2024-01-17 09:47:42 +0100

    Implement verbose KaMPIng variant.

--- sorting.cpp
+++ sorting.cpp
@@ -89,6 +89,7 @@ void parallelSort(MPI_Comm comm, std::vector<T> &data, size_t seed) {
   std::sort(rData.begin(), rData.end());
   rData.swap(data);
 }
+
 template <typename T>
 void parallelSortImproved(MPI_Comm comm, std::vector<T> &data, size_t seed) {
   int rank, size;
@@ -121,6 +122,42 @@ void parallelSortImproved(MPI_Comm comm, std::vector<T> &data, size_t seed) {
   MPI_Alltoallv(data.data(), sCounts.data(), sDispls.data(),
                 kamping::mpi_datatype<T>(), rData.data(), rCounts.data(),
                 rDispls.data(), kamping::mpi_datatype<T>(), comm);
+  std::sort(rData.begin(), rData.end());
+  rData.swap(data);
+}
+
+template <typename T>
+void parallelSortKaMPIngVerbose(MPI_Comm comm_, std::vector<T> &data,
+                                size_t seed) {
+  kamping::Communicator comm(comm_);
+  const size_t oversampling_ratio = 16 * std::log2(comm.size()) + 1;
+  std::vector<T> local_samples(oversampling_ratio);
+  std::sample(data.begin(), data.end(), local_samples.begin(),
+              oversampling_ratio, std::mt19937{seed});
+  std::vector<T> global_samples(local_samples.size() * comm.size());
+  comm.allgather(kamping::send_buf(local_samples),
+                 kamping::recv_buf(global_samples));
+  pick_splitters(comm.size() - 1, oversampling_ratio, global_samples);
+  auto buckets = build_buckets(data, global_samples);
+  std::vector<int> sCounts, sDispls, rCounts(comm.size()), rDispls(comm.size());
+  int send_pos = 0;
+  for (auto &bucket : buckets) {
+    data.insert(data.end(), bucket.begin(), bucket.end());
+    sCounts.push_back(bucket.size());
+    sDispls.push_back(send_pos);
+    send_pos += bucket.size();
+  }
+  comm.alltoall(kamping::send_buf(sCounts), kamping::recv_buf(rCounts));
+
+  // exclusive prefix sum of recv displacements
+  std::exclusive_scan(rCounts.begin(), rCounts.end(), rDispls.begin(), 0);
+  std::vector<T> rData(rDispls.back() + rCounts.back());
+  comm.alltoallv(kamping::send_buf(data), kamping::send_counts(sCounts),
+                 kamping::send_displs(sDispls), kamping::recv_buf(rData),
+                 kamping::recv_counts(rCounts), kamping::recv_displs(rDispls));
+  // MPI_Alltoallv(data.data(), sCounts.data(), sDispls.data(),
+  //               kamping::mpi_datatype<T>(), rData.data(), rCounts.data(),
+  //               rDispls.data(), kamping::mpi_datatype<T>(), comm);
   std::sort(rData.begin(), rData.end());
   rData.swap(data);
 }
@@ -256,7 +293,8 @@ auto generate_data(size_t n_local, size_t seed) -> std::vector<T> {
 }
 
 void log_results(std::string const &json_output_path,
-                 std::string const &algorithm, size_t n_local, size_t seed, bool correct) {
+                 std::string const &algorithm, size_t n_local, size_t seed,
+                 bool correct) {
   std::unique_ptr<std::ostream> output_stream;
   if (json_output_path == "stdout") {
     output_stream = std::make_unique<std::ostream>(std::cout.rdbuf());
@@ -335,6 +373,8 @@ int main(int argc, char *argv[]) {
     do_run(parallelSortImproved<element_type>);
   } else if (algorithm == "kamping") {
     do_run(parallelSortKaMPIng<element_type>);
+  } else if (algorithm == "kamping_verbose") {
+    do_run(parallelSortKaMPIngVerbose<element_type>);
   } else if (algorithm == "boost") {
     do_run(parallelSortBoostMPI<element_type>);
   } else if (algorithm == "rwth") {
