6b20829f24a6dfffead7e862ada5ed9b7ba4b2db
Tim Niklas Uhl <uhl@kit.edu>
2024-01-23 19:13:55 +0100

    Python and rust version.

--- python/sorting.py
+++ python/sorting.py
@@ -2,19 +2,65 @@ from mpi4py import MPI
 import numpy as np
 import sys
 import math
+import argparse
+
+def globally_sorted(comm: MPI.Comm, data, original_data):
+    global_data = comm.gather(data)
+    if global_data is None:
+        global_data = [[]]
+    global_data_original = comm.gather(original_data)
+    if global_data_original is None:
+        global_data_original = [[]]
+    global_data = np.concatenate(global_data)
+    global_data_original = np.concatenate(global_data_original)
+    global_data.sort()
+    global_data_original.sort()
+    return (global_data == global_data_original).all()
+
+def generate_data(n_local, seed):
+    gen = np.random.MT19937(seed + MPI.COMM_WORLD.rank)
+    max_int = np.iinfo(np.uint64).max
+    return np.random.Generator(gen).integers(0, high=max_int, size=n_local, dtype=np.uint64)
+
+def sort(comm: MPI.Comm, data, seed):
+    oversampling_ratio = int(16 * math.log2(comm.size)) + 1;
+    gen = np.random.Generator(np.random.MT19937(seed))
+    local_samples = gen.choice(data, size=oversampling_ratio)
+    global_samples = np.empty(local_samples.size * comm.size, dtype=local_samples.dtype)
+    comm.Allgather(local_samples, global_samples)
+    global_samples.sort()
+    global_samples = global_samples[oversampling_ratio::oversampling_ratio]
+    buckets = np.empty(comm.size, dtype=object)
+    for i in range(len(buckets)):
+        buckets[i] = []
+    bucket_id = np.digitize(data, global_samples)
+    for i in range(len(data)):
+        if buckets[bucket_id[i]] is None:
+            buckets[bucket_id[i]] = []
+        buckets[bucket_id[i]].append(data[i])
+    data = np.concatenate(buckets)
+    scounts = np.array([len(b) for b in buckets], dtype=np.intc)
+    rcounts = np.empty(comm.size, dtype=np.intc)
+    comm.Alltoall(scounts, rcounts)
+    rbuf = np.empty(rcounts.sum(), dtype=data.dtype)
+
+    comm.Alltoallv([data, scounts], [rbuf, rcounts])
+    rbuf.sort()
+    data = rbuf
+
 
 def main():
-    # data = np.random.randint(0, 10, n)
-    data = []
-    for i in range(MPI.COMM_WORLD.size):
-        data += [i] * i
-    comm = MPI.COMM_WORLD
-    data = np.array(data)
-    counts = np.arange(MPI.COMM_WORLD.size)
-    print(data)
-    rbuf = np.empty(comm.rank * comm.size, dtype=np.int64)
-    comm.Alltoallv([data, counts], rbuf)
-    print(rbuf)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--n_local", type=int, required=True)
+    args = parser.parse_args()
+    data = generate_data(args.n_local, args.seed)
+    original_data = data.copy()
+    local_seed = args.seed + MPI.COMM_WORLD.rank + MPI.COMM_WORLD.size
+    sort(MPI.COMM_WORLD, data, local_seed)
+    correct = globally_sorted(MPI.COMM_WORLD, data, original_data)
+    if MPI.COMM_WORLD.rank == 0:
+        print(f"correct: {correct}")
 
 if __name__ == "__main__":
     main()
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,28 @@
+use clap::Parser;
+use mpi::Count;
+use rand::prelude::*;
+use rand::Fill;
+use rand_pcg::Pcg64Mcg;
 use std::ops::Add;
 
 use mpi::{
     datatype::{Partition, PartitionMut},
-    traits::*, topology::SimpleCommunicator,
+    topology::SimpleCommunicator,
+    traits::*,
 };
 
+fn generate_data<T>(n_local: usize, seed: u64) -> Vec<T>
+where
+    T: Default + Clone,
+    [T]: Fill,
+{
+    let mut gen = Pcg64Mcg::seed_from_u64(seed);
+    let mut data = vec![T::default(); n_local];
+    gen.fill(data.as_mut_slice());
+    data
+}
 
-fn exclusive_prefix_sum<T: Add<Output=T> + Copy>(init : T, vec: &[T]) -> Vec<T> {
+fn exclusive_prefix_sum<T: Add<Output = T> + Copy>(init: T, vec: &[T]) -> Vec<T> {
     let mut sum = init;
     let mut out = Vec::with_capacity(vec.len());
     for i in 0..vec.len() {
@@ -16,28 +32,64 @@ fn exclusive_prefix_sum<T: Add<Output=T> + Copy>(init : T, vec: &[T]) -> Vec<T> 
     out
 }
 
-fn bsp_exchange(comm: &SimpleCommunicator, data : &Vec<i32>, scounts : &Vec<i32>) -> Vec<i32> {
+fn sort<T>(comm: &SimpleCommunicator, data: &mut Vec<T>, seed: u64)
+where
+    T: Equivalence + Eq + Ord + Default + Copy
+{
+    let oversampling_ratio = 16 * comm.size().ilog2() + 1;
+    let mut gen = Pcg64Mcg::seed_from_u64(seed);
+    let local_samples = (0..oversampling_ratio)
+        .map(|_| {
+            let idx = gen.gen_range(0..data.len());
+            data[idx]
+        })
+        .collect::<Vec<T>>();
+    let mut global_samples = vec![T::default(); local_samples.len() * (comm.size() as usize)];
+    comm.all_gather_into(&local_samples, &mut global_samples);
+    global_samples.sort();
+    let global_samples  = (0..comm.size() as usize - 1)
+        .map(|i| global_samples[(i + 1) * oversampling_ratio as usize])
+        .collect::<Vec<T>>();
+    let mut buckets = vec![Vec::<T>::new(); comm.size() as usize];
+    for elem in data.iter() {
+        let bucket_idx = global_samples.binary_search(&elem).unwrap_or_else(|x| x);
+        buckets[bucket_idx].push(*elem);
+    }
+    data.clear();
+    let scounts = buckets.iter().map(|x| x.len() as Count).collect::<Vec<Count>>();
+    let mut rcounts = vec![0 as Count; comm.size() as usize];
+    comm.all_to_all_into(&scounts, &mut rcounts);
     let sdispls = exclusive_prefix_sum(0, &scounts);
-    let mut rcounts = vec![0 as i32; comm.size() as usize];
-    comm.all_to_all_into(scounts, &mut rcounts);
     let rdispls = exclusive_prefix_sum(0, &rcounts);
-    let sendbuf = Partition::new(data, scounts.clone(), sdispls);
-    let recv_size = (rcounts.last().unwrap() + rdispls.last().unwrap()) as usize;
-    let mut recv_data = vec![0; recv_size];
-    let mut recvbuf = PartitionMut::new(&mut recv_data, rcounts, rdispls);
-    comm.all_to_all_varcount_into(&sendbuf, &mut recvbuf);
-    recv_data
+    let sbuf =  buckets.into_iter().flatten().collect::<Vec<T>>();
+    let sbuf = Partition::new(&sbuf, scounts, sdispls);
+    data.resize((rcounts.last().unwrap() + rdispls.last().unwrap()) as usize, T::default());
+    let mut rbuf = PartitionMut::new(data, rcounts, rdispls);
+    comm.all_to_all_varcount_into(&sbuf, &mut rbuf);
+    data.sort();
+}
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long, default_value_t = 42)]
+    seed: u64,
+    #[arg(long)]
+    n_local: usize,
 }
 
 fn main() {
     let universe = mpi::initialize().unwrap();
+    let args = Args::parse();
     let comm = universe.world();
-    let mut data = Vec::new();
-    for i in 0..comm.size() {
-        for _ in 0..i {
-            data.push(i);
-        }
-    }
-    let scounts: Vec<i32> = (0..comm.size()).collect();
-    bsp_exchange(&comm, &data, &scounts);
+    let mut data = generate_data::<u64>(args.n_local, args.seed);
+    let local_seed = args.seed + comm.rank() as u64 + comm.size() as u64;
+    sort(&comm, &mut data, local_seed);
+    // let mut data = Vec::new();
+    // for i in 0..comm.size() {
+    //     for _ in 0..i {
+    //         data.push(i);
+    //     }
+    // }
+    // let scounts: Vec<i32> = (0..comm.size()).collect();
+    // bsp_exchange(&comm, &data, &scounts);
 }
