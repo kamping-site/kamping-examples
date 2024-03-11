c76c26ca370ef24fc0803c02d3bb3a9588f23f9a
Tim Niklas Uhl <uhl@kit.edu>
2024-03-11 16:02:42 +0100

    Cleanup ping-pong experiment.

--- type_ping_pong.cpp
+++ type_ping_pong.cpp
@@ -1,11 +1,23 @@
 #include <mpi.h>
 
 #include <CLI/CLI.hpp>
+#include <cereal/types/utility.hpp>
+#include <cereal/types/vector.hpp>
 #include <cstddef>
+#include <kamping/communicator.hpp>
+#include <kamping/mpi_datatype.hpp>
+#include <kamping/p2p/recv.hpp>
+#include <kamping/p2p/send.hpp>
 #include <utility>
 #include <vector>
 
-enum class Type { create_struct, pair_as_bytes, contiguous_type, builtin };
+enum class Type {
+  create_struct,
+  pair_as_bytes,
+  contiguous_type,
+  serialization,
+  builtin
+};
 
 std::string to_string(Type t) {
   switch (t) {
@@ -15,14 +27,119 @@ std::string to_string(Type t) {
       return "pair_as_bytes";
     case Type::contiguous_type:
       return "contiguous_type";
+    case Type::serialization:
+      return "serialization";
     case Type::builtin:
       return "builtin";
   }
   return "unknown";
 }
 
+void log_result(std::vector<double>& times, std::size_t n_reps,
+                std::size_t data_size, Type mpi_type_constructor,
+                std::string const& value_type,
+                const std::string& json_output_path) {
+  if (kamping::comm_world().is_root()) {
+    MPI_Reduce(MPI_IN_PLACE, times.data(), static_cast<int>(n_reps), MPI_DOUBLE,
+               MPI_MAX, 0, MPI_COMM_WORLD);
+  } else {
+    MPI_Reduce(times.data(), nullptr, static_cast<int>(n_reps), MPI_DOUBLE,
+               MPI_MAX, 0, MPI_COMM_WORLD);
+  }
+  if (kamping::comm_world().is_root()) {
+    std::unique_ptr<std::ostream> out;
+    if (json_output_path == "stdout") {
+      out = std::make_unique<std::ostream>(std::cout.rdbuf());
+    } else {
+      std::ofstream file_output(json_output_path);
+      out = std::make_unique<std::ofstream>(std::move(file_output));
+    }
+    double average_time = std::accumulate(times.begin(), times.end(), 0.0) /
+                          static_cast<double>(n_reps);
+    auto [min_time, max_time] = std::minmax_element(times.begin(), times.end());
+    *out << "RESULT ";
+    *out << "n_reps=" << n_reps << " data_size=" << data_size
+         << " mpi_type_constructor=" << to_string(mpi_type_constructor)
+         << " value_type=" << value_type << " ";
+    *out << "average_time=" << average_time << " min_time=" << *min_time
+         << " max_time=" << *max_time << "\n";
+  }
+}
+
+template <typename value_type>
+auto benchmark(kamping::Communicator<> const& comm, std::size_t n_reps,
+               std::size_t data_size, Type mpi_type_constructor)
+    -> std::vector<double> {
+  std::vector<value_type> data(data_size);
+  MPI_Datatype type;
+  int num_elements;
+
+  switch (mpi_type_constructor) {
+    case Type::create_struct: {
+      type = kamping::struct_type<value_type>::data_type();
+      kamping::mpi_env.commit_and_register(type);
+      num_elements = static_cast<int>(data.size());
+      break;
+    }
+    case Type::pair_as_bytes: {
+      type = kamping::byte_serialized<value_type>::data_type();
+      kamping::mpi_env.commit_and_register(type);
+      num_elements = static_cast<int>(data.size());
+      break;
+    }
+    case Type::contiguous_type:
+      MPI_Type_contiguous(
+          static_cast<int>(data_size) * static_cast<int>(sizeof(value_type)),
+          MPI_BYTE, &type);
+      kamping::mpi_env.commit_and_register(type);
+      num_elements = 1;
+      break;
+    case Type::builtin:
+      type = MPI_BYTE;
+      num_elements = static_cast<int>(data.size() * sizeof(value_type));
+      break;
+    case Type::serialization:
+      break;
+  }
+  std::vector<double> times(n_reps);
+  for (std::size_t i = 0; i < n_reps; i++) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start = MPI_Wtime();
+    if (comm.is_root()) {
+      if (mpi_type_constructor != Type::serialization) {
+        comm.send(kamping::send_buf(data), kamping::send_type(type),
+                  kamping::send_count(num_elements), kamping::destination(1));
+        comm.recv(kamping::recv_buf(data), kamping::recv_type(type),
+                  kamping::recv_count(num_elements), kamping::source(1),
+                  kamping::tag(comm.default_tag()));
+      } else {
+        comm.send(kamping::send_buf(kamping::as_serialized(data)),
+                  kamping::destination(1));
+        comm.recv(kamping::recv_buf(kamping::as_deserializable(data)),
+                  kamping::source(1), kamping::tag(comm.default_tag()));
+      }
+    } else if (comm.rank() == 1) {
+      if (mpi_type_constructor != Type::serialization) {
+        comm.recv(kamping::recv_buf(data), kamping::recv_type(type),
+                  kamping::recv_count(num_elements), kamping::source(0),
+                  kamping::tag(comm.default_tag()));
+        comm.send(kamping::send_buf(data), kamping::send_type(type),
+                  kamping::send_count(num_elements), kamping::destination(0));
+      } else {
+        comm.recv(kamping::recv_buf(kamping::as_deserializable(data)),
+                  kamping::source(0), kamping::tag(comm.default_tag()));
+        comm.send(kamping::send_buf(kamping::as_serialized(data)),
+                  kamping::destination(0));
+      }
+    }
+    double end = MPI_Wtime();
+    times[i] = end - start;
+  }
+  return times;
+}
+
 auto main(int argc, char* argv[]) -> int {
-  MPI_Init(&argc, &argv);
+  kamping::Environment env;
   CLI::App app{"MPI datatype ping-pong benchmark"};
   std::size_t n_reps;
   std::size_t data_size;
@@ -37,92 +154,19 @@ auto main(int argc, char* argv[]) -> int {
           {"create_struct", Type::create_struct},
           {"pair_as_bytes", Type::pair_as_bytes},
           {"contiguous_type", Type::contiguous_type},
+          {"serialization", Type::serialization},
           {"builtin", Type::builtin}}));
   std::string json_output_path = "stdout";
   app.add_option("--json_output_path", json_output_path);
   CLI11_PARSE(app, argc, argv);
 
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  using value_type = int;
-  std::vector<value_type> data(data_size);
-  MPI_Datatype type;
-  int num_elements;
-  switch (mpi_type_constructor) {
-    case Type::create_struct: {
-      // value_type t{};
-      // MPI_Aint base;
-      // MPI_Get_address(&t, &base);
-      // MPI_Aint disp[2];
-      // MPI_Get_address(&t.first, &disp[0]);
-      // MPI_Get_address(&t.second, &disp[1]);
-      // disp[0] = MPI_Aint_diff(disp[0], base);
-      // disp[1] = MPI_Aint_diff(disp[1], base);
-      // MPI_Datatype types[2] = {MPI_INT, MPI_INT};
-      // int blocklens[2] = {1, 1};
-      int blocklens = 1;
-      MPI_Aint disp = 0;
-      MPI_Datatype types = MPI_INT;
-      MPI_Type_create_struct(1, &blocklens, &disp, &types, &type);
-      // MPI_Type_contiguous(2, MPI_INT, &type);
-      MPI_Type_commit(&type);
-      num_elements = static_cast<int>(data.size());
-      break;
-    }
-    case Type::pair_as_bytes: {
-      MPI_Type_contiguous(sizeof(value_type), MPI_BYTE, &type);
-      MPI_Type_commit(&type);
-      num_elements = static_cast<int>(data.size());
-      break;
-    }
-    case Type::contiguous_type:
-      MPI_Type_contiguous(static_cast<int>(data_size) * sizeof(value_type),
-                          MPI_BYTE, &type);
-      MPI_Type_commit(&type);
-      num_elements = 1;
-      break;
-    case Type::builtin:
-      type = MPI_INT;
-      num_elements = static_cast<int>(data.size());
-      break;
-  }
-  std::vector<double> times(n_reps);
-  for (std::size_t i = 0; i < n_reps; i++) {
-    MPI_Barrier(MPI_COMM_WORLD);
-    double start = MPI_Wtime();
-    if (rank == 0) {
-      MPI_Send(data.data(), num_elements, type, 1, 0, MPI_COMM_WORLD);
-      MPI_Recv(data.data(), num_elements, type, 1, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
-    } else if (rank == 1) {
-      MPI_Recv(data.data(), num_elements, type, 0, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
-      MPI_Send(data.data(), num_elements, type, 0, 0, MPI_COMM_WORLD);
-    }
-    double end = MPI_Wtime();
-    times[i] = end - start;
-  }
-  if (rank == 0) {
-    MPI_Reduce(MPI_IN_PLACE, times.data(), static_cast<int>(n_reps), MPI_DOUBLE,
-               MPI_MAX, 0, MPI_COMM_WORLD);
-  } else {
-    MPI_Reduce(times.data(), nullptr, static_cast<int>(n_reps), MPI_DOUBLE,
-               MPI_MAX, 0, MPI_COMM_WORLD);
-  }
-  if (rank == 0) {
-    auto& out = std::cout;
-    // std::ofstream out(json_output_path);
-    double average_time =
-        std::accumulate(times.begin(), times.end(), 0.0) / n_reps;
-    auto [min_time, max_time] = std::minmax_element(times.begin(), times.end());
-    out << "RESULT ";
-    out << "n_reps=" << n_reps << " data_size=" << data_size
-        << " mpi_type_constructor=" << to_string(mpi_type_constructor) << " ";
-    out << "average_time=" << average_time << " min_time=" << *min_time
-        << " max_time=" << *max_time << "\n";
-  }
-  MPI_Type_free(&type);
-  MPI_Finalize();
+  kamping::Communicator comm;
+
+  auto times = benchmark<std::pair<std::int32_t, int64_t>>(
+      comm, n_reps, data_size, mpi_type_constructor);
+  log_result(times, n_reps, data_size, mpi_type_constructor, "std::pair<int32_t,int64_t>", json_output_path);
+  times = benchmark<std::pair<std::int64_t, int64_t>>(
+      comm, n_reps, data_size, mpi_type_constructor);
+  log_result(times, n_reps, data_size, mpi_type_constructor, "std::pair<int64_t,int64_t>", json_output_path);
   return 0;
 }
