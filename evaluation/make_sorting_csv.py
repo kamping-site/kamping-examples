4d6f966a9d51eb0c18acfb137e40bc2f56444cc3
Matthias Schimek <matthias.schimek@kit.edu>
2024-06-04 15:30:27 +0200

    adapt ploting scripts

--- evaluation/make_sorting_csv.py
+++ evaluation/make_sorting_csv.py
@@ -5,10 +5,18 @@ import pandas as pd
 import argparse
 from pathlib import Path
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path",help = "path to base directory where the experiments are located.")
-    parser.add_argument("--experiments", nargs="*", help = "name of the experiments")
+    parser.add_argument(
+        "--path", help="path to base directory where the experiments are located."
+    )
+    parser.add_argument("--experiments", nargs="*", help="name of the experiments")
+    parser.add_argument(
+        "--mpi-type",
+        help="name of MPI implementation used in the experiments",
+        default="unspecified",
+    )
     args = parser.parse_args()
 
     value_paths = {
@@ -16,7 +24,7 @@ def main():
         "algorithm": ["info", "algorithm"],
         "n_local": ["info", "n_local"],
         "correct": ["info", "correct"],
-        'time': ['root', 'total_time', 'statistics', 'max']
+        "time": ["root", "total_time", "statistics", "max"],
     }
 
     df = []
@@ -25,11 +33,19 @@ def main():
         if not experiment_path.exists():
             print("The experiment directory {} doesn't exist".format(experiment_path))
             sys.exit(1)
-        df.append(logparser.read_logs_from_directory(experiment_path, "intel", value_paths))
+        df.append(logparser.read_logs_from_directory(experiment_path, "-", value_paths))
 
     df = pd.concat(df)
-    df_wide = df.query('iteration > 0').pivot_table(index="p", columns=["algorithm"] , values="time", aggfunc='mean')
-    df_wide.to_csv(sys.stdout, index = True)
+
+    cols = ["p", "algorithm", "mpi_type", "iteration", "time"]
+
+    # df_wide = df.query("iteration > 0").pivot_table(
+    #    index="p", columns=["algorithm"], values="time", aggfunc="mean"
+    # )
+    df = df.sort_values(cols)
+    df = df[cols]
+    df.to_csv(sys.stdout, index=False)
+
 
 if __name__ == "__main__":
     main()
